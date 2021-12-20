d8b109ff19cf1d37e3a391f06c02c0de7607b822
etoyz <2905226519@qq.com>
2021-12-20 16:07:12 +0800

    Opt: comment

--- Client/ClientMain.cpp
+++ Client/ClientMain.cpp
@@ -1,60 +1,52 @@
-//Don't forget to link libws2_32.a to your program as well
 #include <winsock2.h>
 #include <iostream>
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-SOCKET server_socket_descriptor; //Socket handle
+SOCKET server_socket_descriptor;
 
 bool SendToHost(int PortNo, char* IPAddress, char* Msg)
 {
-	//Start up Winsock…
+	//使用Windows系统API
 	WSADATA wsadata;
-
-	/*
-	* If successful, the WSAStartup function returns zero. Otherwise, it returns one of the error codes
-	*/
 	if (WSAStartup(0x0202, &wsadata) != 0)
 		return false;
 
-	//Fill out the information needed to initialize a socket…
-	SOCKADDR_IN target;               //Socket address information
-
-	target.sin_family = AF_INET;      // address family Internet
-	target.sin_port = htons(PortNo); //Port to connect on
-	target.sin_addr.s_addr = inet_addr(IPAddress); //Target IP
-
+	// 填写初始化套接字所需的信息
+	SOCKADDR_IN target;
+	target.sin_family = AF_INET;
+	target.sin_port = htons(PortNo);
+	target.sin_addr.s_addr = inet_addr(IPAddress);
+	// 创建套接字
 	server_socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Create socket
 	if (server_socket_descriptor == INVALID_SOCKET)
 	{
 		return false; //Couldn't create the socket
 	}
 
-	//Try connecting...
+	// 尝试与服务器连接
 	if (connect(server_socket_descriptor, (SOCKADDR*)&target, sizeof(target)) == SOCKET_ERROR)
 	{
-		return false;                 //Couldn't connect
+		return false;
 	}
 	else {
 		send(server_socket_descriptor, Msg, strlen(Msg), 0);
-
-		char buff[100] = { 0 };
-		int size = recv(server_socket_descriptor, buff, strlen(buff), MSG_WAITALL);
-		cout << buff << endl;
-
-		return true;                  //Success
+		return true;
 	}
 }
 
-void HandleDataFromHost()
+/*
+* 处理从服务器传回的控制数据
+*/
+void HandleDataFromServer()
 {
 	char buff[100] = { 0 };
 	int size = recv(server_socket_descriptor, buff, sizeof(buff), MSG_WAITALL);
 	cout << "接受到控制信息：" << buff << endl;
 	cout << "----------------------" << endl;
 	cout << "发出控制信号如下:\n";
-	if(buff[0] == '1')
+	if (buff[0] == '1')
 		cout << "关闭所有设备\n";
 	if (buff[1] == '1')
 		cout << "关闭门窗\n";
@@ -64,7 +56,7 @@ void HandleDataFromHost()
 		cout << "关闭卫生间灯光\n";
 	if (buff[4] == '1') {
 		cout << "打开空调\n";
-		if(buff[5] == '0')
+		if (buff[5] == '0')
 			cout << "开启空调制冷，";
 		else
 			cout << "开启空调制热，";
@@ -75,25 +67,17 @@ void HandleDataFromHost()
 				cout << "风速为低风";
 			else
 				cout << "风速为中风";
-		} else 
+		}
+		else
 			cout << "风速为高风";
-		cout << "\n控制信号发送完毕！\n";
+		cout << "\n控制信号解析完毕！\n";
 	}
-		
-}
 
-//CLOSECONNECTION – shuts down the socket and closes any connection on it
-void CloseConnection()
-{
-	//Close the socket if it exists
-	if (server_socket_descriptor)
-		closesocket(server_socket_descriptor);
-
-	WSACleanup();                     //Clean up Winsock
 }
 
 /*
 	[1][3][3][2][[1][3][1][1]][1][1]
+	共 17位
 	房间状态：有无人。只上传
 	温度传感器：采集房间温度信息。只上传
 	湿度传感器：采集房间湿度信息。只上传
@@ -104,13 +88,18 @@ void CloseConnection()
 */
 int main()
 {
-	char ip[] = "127.0.0.1";
+	char ip[] = "192.168.1.151";
 	char msg[] = "111111111111111111";
 	if (SendToHost(81, ip, msg))
 		cout << "发送传感器数据：" << msg << endl;
 	else
+	{
 		cout << "Sent failed!" << endl;
-	HandleDataFromHost();
-	//CloseConnection();
+		exit(1);
+	}
+	HandleDataFromServer();
+	// 释放资源
+	closesocket(server_socket_descriptor);
+	WSACleanup();
 	system("pause");
 }
--- Server/ServerMain.cpp
+++ Server/ServerMain.cpp
@@ -6,17 +6,16 @@
 
 using namespace std;
 
-#define IP "127.0.0.1" //绑定的IP
-#define port 81 //监听的端口，可以在范围内自由设定
+#define IP "192.168.1.151" //绑定的IP
+#define port 81 //监听的端口
 #define MAXIMUM_CONNECTION 10 // 最大连接数
-fd_set client_socket_descriptors; // 连接池
 
 /*
-* 解析客户机请求的信息, 并发送控制数据
+* 解析客户机请求的数据, 并发送控制数据
 */
 string parse(string data, int size)
 {
-	// Output to the terminal
+	// 输出解析信息到标准输出设备
 	cout << "请求数据:\n" << data << endl;
 	cout << "共 " << size << " 字节。" << endl;
 
@@ -74,52 +73,56 @@ string parse(string data, int size)
 	cout << "----------------------" << endl;
 	cout << "根据解析结果发送控制信息: " << endl;
 	if (data[0] == '0') { // 无人
-		cout << "是否关闭所有设备\n";
+		cout << "关闭所有设备\n";
 		cmd_to_client += "1";
 	}
 	else cmd_to_client += "0";
 	if (data[0] == '1' && data[9] == '1') { // 有人且空调开启
-		cout << "是否关闭门窗\n";
+		cout << "关闭门窗\n";
 		cmd_to_client += "1";
 	}
 	else cmd_to_client += "0";
 	if (data[0] == '1' && data[16] == '0') { // 有人且窗帘关闭
-		cout << "是否打开卧室灯光\n";
+		cout << "打开卧室灯光\n";
 		cmd_to_client += "1";
 	}
 	else cmd_to_client += "0";
 	if (data[0] == '1' && data[16] == '1') { // 有人且窗帘打开
-		cout << "是否关闭卫生间灯光\n";
+		cout << "关闭卫生间灯光\n";
 		cmd_to_client += "1";
 	}
 	else cmd_to_client += "0";
 	if (data[0] == '1') { // 有人
-		cmd_to_client += "1";
 		string tempe_str = string(1, data[10]) + string(1, data[11]) + '.' + string(1, data[12]);
 		float t;
 		sscanf(tempe_str.c_str(), "%f", &t);
 		cout << "温度为 " + tempe_str + " ℃，";
 		if (t > 30) { // 且温度高于30度
 			cout << "开启空调制冷，设定温度为24度，风速为中风";
-			cmd_to_client += "02401";
+			cmd_to_client += "102401";
 		}
 		else if (t < 15) { // 且温度低于15度
 			cout << "开启空调制热，设定温度为26度，风速为高风";
-			cmd_to_client += "12610";
+			cmd_to_client += "112610";
+		}
+		else {
+			cmd_to_client += "0XXXXX";
 		}
 	}
 	else cmd_to_client += "0XXXXX";
 
-	cout << "\n发送控制信息： " + cmd_to_client;
+	cout << "\n发送控制数据： " + cmd_to_client;
 	return cmd_to_client;
 }
 
 int main()
 {
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//使用Windows系统API
+	WSADATA wsadata;
+	if (WSAStartup(0x0202, &wsadata) != 0)
+		return false;
 
-	//1.创建一个socket套接字
+	// 创建一个socket套接字
 	SOCKET local_socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (local_socket_descriptor == INVALID_SOCKET)
 	{
@@ -129,7 +132,7 @@ int main()
 	cout << "Server local socket created successfully!" << endl;
 
 	/*
-	* 2.填写初始化套接字所需的信息
+	* 填写初始化套接字所需的信息
 	* sockaddr_in结构体：可以存储一套网络地址（包括IP与端口）,此处存储本机IP地址与本地的一个端口
 	*/
 	struct sockaddr_in local_addr;
@@ -138,7 +141,7 @@ int main()
 	local_addr.sin_addr.s_addr = inet_addr(IP); //绑定特定IP地址
 
 	/*
-	* 3.初始化套接字
+	* 初始化套接字
 	* bind()： 将一个网络地址与一个套接字绑定，此处将服务器本地地址绑定到一个套接字上
 	*/
 	int res = bind(local_socket_descriptor, (struct sockaddr*)&local_addr, sizeof(local_addr));
@@ -149,40 +152,43 @@ int main()
 	}
 	cout << "Socket initialization successfully!" << endl;
 
-	//4.listen()函数：监听试图连接本机的客户端
-	//参数二：监听的进程数
+	/* 
+	* listen()函数：监听试图连接本机的客户端
+	* 参数二：监听的进程数
+	*/
 	listen(local_socket_descriptor, MAXIMUM_CONNECTION);
 	cout << "Waiting for a connection...." << endl;
 
 	while (true)//循环接收客户端的请求
 	{
-		//5.创建一个sockaddr_in结构体，用来存储客户机的地址
+		// 5.创建一个sockaddr_in结构体，用来存储客户机的地址
 		struct sockaddr_in client_addr;
 		socklen_t len = sizeof(client_addr);
-		//6.accept()函数：阻塞运行，直到收到某一客户机的连接请求，并返回客户机的描述符
+		// accept()函数：阻塞运行，直到收到某一客户机的连接请求，并返回客户机的描述符
 		SOCKET client_socket_descriptor = accept(local_socket_descriptor, (struct sockaddr*)&client_addr, &len);
-		//WSAAsyncSelect(local_socket_descriptor,NULL,NULL,FD_ACCEPT)
 		if (client_socket_descriptor == SOCKET_ERROR)
 		{
 			cout << "Unable to create client socket!\n" << endl;
 			exit(-1);
 		}
 
-		//7.输出客户机的信息
+		// 输出客户机的信息
 		string ip = inet_ntoa(client_addr.sin_addr);
 		cout << "\n\n========================================================" << endl;
 		cout << "客户机: " << ip << " 连接到本服务器!" << endl;
-		cout << client_socket_descriptors.fd_count;
+		//cout << client_socket_descriptors.fd_count;
 
-		//8.处理客户机请求的信息
+		// 处理客户机请求的数据
 		char buff[100] = { 0 };
 		int size = recv(client_socket_descriptor, buff, sizeof(buff), 0);
-		string cmd_to_client = parse(buff, size);
+		string recv_data;
+		recv_data.assign(buff, buff + size);
+		string cmd_to_client = parse(recv_data, size);
 
-		//9.使用第6步accept()返回socket描述符，即客户机的描述符，进行通信。
-		send(client_socket_descriptor, cmd_to_client.c_str(), cmd_to_client.size(), 0);//返回给客户端信息
+		// 使用第6步accept()返回socket描述符，即客户机的描述符，进行通信。
+		send(client_socket_descriptor, cmd_to_client.c_str(), cmd_to_client.size(), 0); //返回给客户端数据
 
-		//10.关闭客户端的socket
+		// 关闭该客户的socket
 		shutdown(client_socket_descriptor, 2);
 	}
 	shutdown(local_socket_descriptor, 2);
